b27c4ca8aaf6177081dfae1aff40b54369a6c988
enoshima7 <duanjipeng0328@gmail.com>
2020-02-17 17:38:15 +0800

    精灵图片 icon导航栏

--- web/src/components/Home.vue
+++ web/src/components/Home.vue
@@ -27,6 +27,23 @@
         slot="pagination"
       ></div>
     </swiper>
+    <!-- end of swiper -->
+    <div class="nav-icons bg-white mt-3 text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div
+          class="nav-item mb-3"
+          v-for="n in 13"
+          :key="n"
+        >
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -53,6 +70,17 @@ export default {
     background: map-get($map: $colors, $key: "white");
     &.swiper-pagination-bullet-active {
       background: map-get($colors, "info");
+    }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
